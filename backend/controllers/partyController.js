1b57b34a5eca0158ef059fdde5f423b6a9f0086e
Juliano Carneiro <julianoomc@gmail.com>
2023-07-15 03:26:39 -0300

    creating other party requests

--- backend/controllers/partyController.js
+++ backend/controllers/partyController.js
@@ -32,7 +32,79 @@ const partyController = {
         } catch (error) {
             console.log(error)
         }
-    }    
+    },
+
+    getAll: async(req, res) => {
+        try {
+            const parties = await PartyModel.find()
+            res.json(parties)
+        } catch (error) {
+            console.log(error)
+        }
+    },
+
+    get: async(req, res) => {
+        try {
+            const id = req.params.id
+            const party = await PartyModel.findById(id)
+
+            if(!party) {
+                res.status(404).json({msg: 'Party not found.'})
+                return
+            }
+
+            res.json(party)
+        } catch (error) {
+            console.log(error)
+        }
+    },
+
+    delete: async(req, res) => {
+        try {
+            const id = req.params.id
+            const party = await PartyModel.findById(id)
+
+            if(!party) {
+                res.status(404).json({msg: 'Party not found.'})
+                return
+            }
+
+            const deletedParty = await PartyModel.findByIdAndDelete(id)
+            res.status(200).json({deletedParty, msg: 'Party successfully removed.'})
+        } catch (error) {
+            console.log(error)
+        }
+    },
+
+    update: async(req, res) => {
+        try {
+            const id = req.params.id
+            const party = {
+                title: req.body.title,
+                author: req.body.author,
+                description: req.body.description,
+                budget: req.body.budget,
+                image: req.body.image,
+                services: req.body.services
+            }
+
+            if(party.services && !isPartyBudgetEnough(party.budget, party.services)) {
+                res.status(406).json({msg: 'Not enough budget.'})
+                return
+            }
+
+            const updatedParty = await PartyModel.findByIdAndUpdate(id, party)
+
+            if(!updatedParty) {
+                res.status(404).json({msg: 'Party not found.'})
+                return
+            }
+
+            res.status(200).json({updatedParty, msg: 'Party successfully updated.'})
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
 
 module.exports = partyController
